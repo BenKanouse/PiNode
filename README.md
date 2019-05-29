c80167039ffe0cff2a838f3a1a97256b8d9c8be6
Ben <kanobt61@gmail.com>
2019-05-29 13:22:04 -0400

    Calculate Pi using the Monte Carlo Method
    It defaults to 100,000,000 simulations.
    https://en.wikipedia.org/wiki/Monte_Carlo_method

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # PiNode
+
+
+```
+$ time npm run start
+
+> pi-node@1.0.0 prestart /Users/Ben/Projects/PiNode
+> npm run build
+
+
+> pi-node@1.0.0 build /Users/Ben/Projects/PiNode
+> tsc
+
+
+> pi-node@1.0.0 start /Users/Ben/Projects/PiNode
+> node dist/pi.js
+
+Pi: 3.14157704
+
+real	0m5.297s
+user	0m6.144s
+sys	0m0.360s
+```
--- src/pi.ts
+++ src/pi.ts
@@ -1,7 +1,17 @@
-function greeter(person: string) {
-  return `Hello ${person}!`
+function pi(sampleSize: number) {
+  let insideCircle: number = 0;
+
+  for (let i: number = 0; i < sampleSize; i++) {
+    const x: number = Math.random();
+    const y: number = Math.random();
+    if (Math.sqrt(x * x + y * y) < 1) {
+      insideCircle++
+    }
+  }
+
+  const pi: number = (insideCircle / sampleSize) * 4;
+
+  return `Pi: ${pi}`
 }
 
-const hello = 'Node Hero';
-
-console.log(greeter(hello));
+console.log(pi(100_000_000));
